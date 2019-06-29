2b91fd32242e8ab16bd408654b804ee6c1db513f
Ravi Kumar <ravi.kumar4@gmail.com>
2019-06-29 15:39:35 -0400

    first commit

---
+++ java-exercise/src/main/java/com/pronixinc/training/java_exercise/App.java
@@ -0,0 +1,13 @@
+package com.pronixinc.training.java_exercise;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
